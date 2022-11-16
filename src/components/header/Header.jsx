53c7de28305d155f480db5280ab4e9b8d8ab35c2
Ajhay Dechos <dchaos10600@gmail.com>
2022-11-17 05:41:39 +0800

    Deployed website

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -20,12 +20,13 @@ const Header = () => {
       <h5 className="text-light">Hi There! I'm</h5>
       <h1 className="myname animate__animated animate__pulse animate__slow	2s animate__infinite	infinite ">Ajhay</h1>
       <h5 className="s_e"> Software Engineer</h5>
-
+      
       <CTA/>
       <HeaderSocials/>
 
       <div className="me" data-aos="zoom-in" data-aos-mirror="true" data-aos-once="false" data-aos-duration="2000">
         <img src={Me} alt="me" />
+        <h5 className="s_e">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi sunt dolorem ex ipsam aliquid dolores ratione amet. Esse molestiae magnam hic expedita harum aliquid, ipsum tenetur nulla? Beatae, inventore voluptas.</h5>
       </div>
 
       <div className="scroll_Down">
