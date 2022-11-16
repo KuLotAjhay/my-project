dc853cc918f888d801031abcda99d9e9accf0153
Ajhay Dechos <dchaos10600@gmail.com>
2022-11-17 05:30:40 +0800

    Deployed website

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import {BsFacebook} from 'react-icons/bs'
 import {BsLinkedin} from 'react-icons/bs'
 import {BsGithub} from 'react-icons/bs'
-import {BsFacebook} from 'react-icons/bs'
+
 
 const HeaderSocials = () => {
   return (
    <div className="header_Socials"  data-aos="zoom-in-up" data-aos-duration="1800">
-    <a href="https://linkedin.com"><BsLinkedin/></a>
     <a href="https://github.com" ><BsGithub/></a>
     <a href="https://facebook.com"><BsFacebook/></a>
+    <a href="https://linkedin.com"><BsLinkedin/></a>
    </div>
   )
 }
