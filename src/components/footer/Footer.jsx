95902e24c9dc3f3d7f7b1da2f40cbf9ce3a8b08c
Ajhay Dechos <dchaos10600@gmail.com>
2022-11-17 05:16:23 +0800

    deployed website

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,10 +7,10 @@ import {IoLogoGithub} from 'react-icons/io'
 const Footer = () => {
   return (
     <footer>
-      <a href="#" class="footer_logo">AJhay</a>
+      <a href="#home" class="footer_logo">AJhay</a>
 
       <ul className="permalinks">
-        <li><a href="#">Home</a></li>
+        <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
         <li><a href="#services">Services</a></li>
